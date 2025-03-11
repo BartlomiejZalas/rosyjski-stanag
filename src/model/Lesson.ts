ae775f701f00b4f668575502a5bdf1c0bb128be5
Bartłomiej Zalas <bartekzalas@gmail.com>
2025-03-11 23:16:23 +0100

    better top 1000 added (part 1)

--- src/model/Lesson.ts
+++ src/model/Lesson.ts
@@ -11,7 +11,7 @@ export enum Lesson {
     LEKCJA_6 = 'LEKCJA_6',
     LEKCJA_7 = 'LEKCJA_7',
     LEKCJA_8 = 'LEKCJA_8',
-    TOP_1000 = 'TOP_1000',
+    TOP_1000_1 = 'TOP_1000_1',
     MARKED = 'MARKED',
 }
 
