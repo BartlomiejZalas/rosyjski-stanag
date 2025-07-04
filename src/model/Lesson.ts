67424b0ebfa6c834ed6e524ea88971a52ff3fd12
Bartłomiej Zalas <bartekzalas@gmail.com>
2025-07-04 20:58:05 +0200

    lesson 17 introduced

--- src/model/Lesson.ts
+++ src/model/Lesson.ts
@@ -19,6 +19,7 @@ export enum Lesson {
     LEKCJA_14 = 'LEKCJA_14',
     LEKCJA_15 = 'LEKCJA_15',
     LEKCJA_16 = 'LEKCJA_16',
+    LEKCJA_17 = 'LEKCJA_17',
     TOP_1000_1 = 'TOP_1000_1',
     TOP_1000_2 = 'TOP_1000_2',
     STANAG_PISANIE = 'STANAG_PISANIE',
